5979b26d534c9cb00ed942da371a47f50f658bb4
Soomin Jang <jsmsumin2@gmail.com>
2021-11-28 01:43:34 +0900

    menage-member/info, session, inbody 레이아웃

--- client/src/pages/manage-member/index.tsx
+++ client/src/pages/manage-member/index.tsx
@@ -9,6 +9,7 @@ import { useReactiveVar } from '@apollo/client'
 
 interface Member {
 	id: string
+	email: string
 	name: string
 	phone: string
 	gender: string
@@ -48,6 +49,7 @@ const ManageMember: NextPage = () => {
 		}
 		sessionObject[el.membercategory].push({
 			id: el.id,
+			email: el.email,
 			name: el.name,
 			phone: el.phone,
 			gender: el.gender
@@ -212,11 +214,14 @@ const ManageMember: NextPage = () => {
 																	d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
 																/>
 															</svg>
-															{/* <Link href="/manage-member/[emailId]-home"> */}
-															<div className="ml-1 hover:cursor-pointer">
-																{member.name} 회원님
-															</div>
-															{/* </Link> */}
+															<Link
+																href={`/manage-member/${
+																	member.email.split('@')[0]
+																}/info`}>
+																<div className="ml-1 hover:cursor-pointer">
+																	{member.name} 회원님
+																</div>
+															</Link>
 														</div>
 														<svg
 															xmlns="http://www.w3.org/2000/svg"
