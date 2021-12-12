c6f088082953b343d292cb4962a6c93828a133a1
oyeon-kwon <rnjsdhdus93@gmail.com>
2021-12-12 17:45:13 +0900

    유저 세션 상세페이지 쿼리 구현

--- client/src/pages/user/session/index.tsx
+++ client/src/pages/user/session/index.tsx
@@ -4,6 +4,7 @@ import Detail from '../../../components/Detail'
 import Layout from '../../../components/Layout'
 import { gql, useQuery, useMutation, useReactiveVar } from '@apollo/client';
 import UserSession from '../../../../src/graphql/graphql';
+import Link from 'next/link'
 
 // TODO: 날짜, 요일, 시간 쪽을 클릭하면 e.target.children이 빈 배열로 나옴 -> 에러 발생
 // TODO: 세션 디테일 모달을 트레이너 참고해서 변경
@@ -39,43 +40,23 @@ const Session: NextPage = () => {
 
     // TODO: setSessionList(받아온 데이터)
 
-    const [ isDetailOpen, setIsDetailOpen ] = useState(false)
-
-    const [ clickedSession, setClickedSession ] = useState({
-        date: "",
-        day: ""
-    })
-
-    const directSessionDetail = (e: any) => {
-        let date = e.target.children[0].textContent
-        let day = e.target.children[1].textContent
-
-        setClickedSession({
-            date: date,
-            day: day
-        })
-        setIsDetailOpen(!isDetailOpen)
-    }
-
 	return (
         <Layout variant="Web">
 		<div className="flex flex-col m-5 mx-4 my-5 font-IBM text-[12px]">
-                {isDetailOpen === true ? 
-                    <Detail date={clickedSession.date} day={clickedSession.day} isOpen={isDetailOpen} changeOpen={setIsDetailOpen} /> 
-                : 
                 <>
                 <div className="text-[20px] mb-3 font-IBM font-bold">수업 기록</div>
                 {
                     sessionList.map((session) => (
-                        <div onClick={directSessionDetail} className="border border-gray-300 rounded-2xl mb-2 hover:bg-gray-100 hover:cursor-pointer">
-                            <div className="inline-block p-1 mx-1 font-IBM font-medium">{session.date}</div>
-                            <div className="inline-block p-1 mx-1 font-IBM font-medium">{`${session.day}요일`}</div>
-                            <div className="inline-block p-1 mx-1 font-IBM font-medium float-right">{session.time}</div>
-                        </div>
+                        <Link href='/user/session/date'>
+                            <div className="border border-gray-300 rounded-2xl mb-2 hover:bg-gray-100 hover:cursor-pointer">
+                                <div className="inline-block p-1 mx-1 font-IBM font-medium">{session.date}</div>
+                                <div className="inline-block p-1 mx-1 font-IBM font-medium">{`${session.day}요일`}</div>
+                                <div className="inline-block p-1 mx-1 font-IBM font-medium float-right">{session.time}</div>
+                            </div>
+                        </Link>
                     ))
                 }
                 </>
-                }
 		</div>
         </Layout>
 	)
