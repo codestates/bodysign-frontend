63188e6a449ff728af98052087e6729c00443126
Soomin Jang <jsmsumin2@gmail.com>
2021-12-18 13:40:12 +0900

    회원가입 UI, 로직 변경

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -27,13 +27,13 @@ const labelProperties =
 const Signup: NextPage = () => {
 	const router = useRouter()
 	const [areYouTrainer, setAreYouTrainer] = useState(true)
-	const [exerciseTypes, setExerciseTypes] = useState([
-		{ id: 0, name: 'PT샵', status: false },
-		{ id: 1, name: '헬스장', status: false },
-		{ id: 2, name: '크로스핏', status: false },
-		{ id: 3, name: '요가', status: false },
-		{ id: 4, name: '필라테스', status: false },
-		{ id: 5, name: '기타', status: false }
+	const [interestedTypes, setInterestedTypes] = useState([
+		{ id: 0, type: 'PT', name: 'PT샵', status: false },
+		{ id: 1, type: 'Workout', name: '헬스장', status: false },
+		{ id: 2, type: 'Crossfit', name: '크로스핏', status: false },
+		{ id: 3, type: 'Yoga', name: '요가', status: false },
+		{ id: 4, type: 'Pilates', name: '필라테스', status: false },
+		{ id: 5, type: 'Etc', name: '기타', status: false }
 	])
 	const [checkedPersonalInfo, setCheckedPersonalInfo] = useState(false)
 	const loginType = useReactiveVar(loginTypeVar)
@@ -48,7 +48,7 @@ const Signup: NextPage = () => {
 		handleSubmit
 	} = useForm<FormInput>()
 	const onSubmit: SubmitHandler<FormInput> = async data => {
-		// exerciseTypes 필드 추가되면 인풋에 추가한다.
+		// const interestTypes = exerciseTypes.filter(type => type.status)
 		const input = {
 			email: data.email,
 			userName: data.name,
@@ -88,6 +88,8 @@ const Signup: NextPage = () => {
 							<input
 								className="w-full h-12 p-3 mt-1 border"
 								type="text"
+								disabled={loginType === 'google'}
+								// defaultValue 소셜 회원가입 이메일
 								{...register('email', {
 									required: true,
 									pattern:
@@ -106,6 +108,7 @@ const Signup: NextPage = () => {
 							<input
 								className="w-full h-12 p-3 mt-1 border"
 								type="password"
+								disabled={loginType === 'google'}
 								{...register('password', {
 									required: true,
 									minLength: 8
@@ -127,34 +130,6 @@ const Signup: NextPage = () => {
 									required: true
 								})}
 							/>
-						</div>
-
-						<div className="mt-4">
-							<label>생년월일</label>
-							<input
-								className="w-full h-12 p-3 mt-1 border"
-								type="date"
-								{...register('birth', {
-									required: true
-								})}
-							/>
-						</div>
-
-						<div className="mt-4">
-							<label>휴대폰 번호</label>
-							<input
-								className="w-full h-12 p-3 mt-1 border"
-								type="text"
-								{...register('phone', {
-									required: true,
-									pattern: /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/
-								})}
-							/>
-							{errors.phone && (
-								<div className="text-[16px] text-red-500 mt-1 text-center">
-									붙임표(-)는 제외하고 입력해주세요.
-								</div>
-							)}
 						</div>
 
 						<div className="mt-4">
@@ -237,12 +212,12 @@ const Signup: NextPage = () => {
 									Pick a few types that you're interested in.
 								</div>
 								<div className="flex flex-wrap justify-between py-2">
-									{exerciseTypes.map(type => {
+									{interestedTypes.map(type => {
 										return (
 											<React.Fragment key={type.id}>
 												<span
 													className={`p-1 ring-1 ring-green-200 flex items-center ${
-														exerciseTypes[type.id].status
+														interestedTypes[type.id].status
 															? 'bg-yellow-100 '
 															: ''
 													}`}
@@ -253,9 +228,9 @@ const Signup: NextPage = () => {
 															e.target instanceof HTMLElement
 														) {
 															const idx = Number(e.target.dataset.id)
-															if (exerciseTypes[idx].status) {
-																setExerciseTypes(
-																	exerciseTypes.map(type => {
+															if (interestedTypes[idx].status) {
+																setInterestedTypes(
+																	interestedTypes.map(type => {
 																		if (type.id === idx) {
 																			type.status = false
 																		}
@@ -263,8 +238,8 @@ const Signup: NextPage = () => {
 																	})
 																)
 															} else {
-																setExerciseTypes(
-																	exerciseTypes.map(type => {
+																setInterestedTypes(
+																	interestedTypes.map(type => {
 																		if (type.id === idx) {
 																			type.status = true
 																		}
@@ -274,7 +249,7 @@ const Signup: NextPage = () => {
 															}
 														}
 													}}>
-													{exerciseTypes[type.id].status ? (
+													{interestedTypes[type.id].status ? (
 														<svg
 															xmlns="http://www.w3.org/2000/svg"
 															className="w-4 h-4 text-green-500"
@@ -296,7 +271,37 @@ const Signup: NextPage = () => {
 									})}
 								</div>
 							</div>
-						) : null}
+						) : (
+							<>
+								<div className="mt-4">
+									<label>생년월일</label>
+									<input
+										className="w-full h-12 p-3 mt-1"
+										type="date"
+										{...register('birth', {
+											required: true
+										})}
+									/>
+								</div>
+
+								<div className="mt-4">
+									<label>휴대폰 번호</label>
+									<input
+										className="w-full h-12 p-3 mt-1 border"
+										type="text"
+										{...register('phone', {
+											required: true,
+											pattern: /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/
+										})}
+									/>
+									{errors.phone && (
+										<div className="text-[16px] text-red-500 mt-1 text-center">
+											붙임표(-)는 제외하고 입력해주세요.
+										</div>
+									)}
+								</div>
+							</>
+						)}
 
 						<div className="flex items-center mt-4">
 							<input
